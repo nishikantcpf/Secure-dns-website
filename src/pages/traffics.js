094e194acc248d536ada238d1bbf222ad4eeef4b
nishikantsingh2 <nishikantsmarty@gmail.com>
2024-07-07 14:20:37 +0530

    added trafic page

--- src/pages/traffics.js
+++ src/pages/traffics.js
@@ -87,26 +87,92 @@ const Traffics = () => {
                         <div className="menuContainer">
                             <Menu />
                         </div>
-                        {userData === null ? <></> : <>
-                            <div className="contentContainer" style={{ borderRadius: '20px', }}>
-                                <div style={{ backgroundColor: "#EADEDA", borderRadius: '20px', padding: '20px' }}>
-                                    <div className="myTable" style={{ height: 600, width: "100%" }}>
-                                        <DataGrid
+                        {/* {userData === null ? <></> : <> */}
+                        <div className="contentContainer" style={{ borderRadius: '20px'}}>
+                            <div style={{ backgroundColor: "white", borderRadius: '20px', padding: '20px' }}>
+                                <div className="myTable" style={{ height: 550, width: "100%" }}>
+                                    {/* <DataGrid
                                             rows={userData}
                                             columns={columns}
                                             hideScrollbar={true}
                                
 
-                                        />
-                                      
+                                        /> */}
+                                    <div class="container">
+                                        
+                                       
+                                        <table class="table" style={{paddingTop:"20px"}}>
+                                            <thead>
+                                                <tr>
+                                                    <th>Unique Domain</th>
+                                                    <th>Status</th>
+                                                   
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr style={{ backgroundColor: "#dff0d8" }}>
+                                                    <td >sb.scorecardresearch.com</td>
+                                                    <td style={{ color: "green" }}><i class="fa fa-circle" aria-hidden="true"></i></td>
+                                                </tr>
+                                                <tr style={{ backgroundColor: "#dff0d8" }}>
+                                                    <td>www.googletagmanager.com</td>
+                                                    <td style={{ color: "green" }}><i class="fa fa-circle" aria-hidden="true"></i></td>
 
+                                                </tr>
+                                                <tr style={{ backgroundColor: "#dff0d8" }}>
+                                                    <td>tunnel.googlezip.net</td>
+                                                    <td style={{ color: "green" }}><i class="fa fa-circle" aria-hidden="true"></i></td>
+                                                </tr>
+                                                <tr style={{ backgroundColor: "#dff0d8" }}>
+                                                    <td>encrypted-tbn2.gstatic.com</td>
+                                                    <td style={{ color: "green" }}><i class="fa fa-circle" aria-hidden="true"></i></td>
+                                                </tr>
+                                                <tr style={{ backgroundColor: "#dff0d8" }}>
+                                                    <td>www-bikedekho-com.cdn.ampproject.org</td>
+                                                    <td style={{ color: "green" }}><i class="fa fa-circle" aria-hidden="true"></i></td>
+                                                </tr>
+
+                                                <tr style={{ backgroundColor: "#dff0d8" }}>
+                                                    <td>connect.facebook.net</td>
+                                                    <td style={{ color: "green" }}><i class="fa fa-circle" aria-hidden="true"></i></td>
+                                                </tr>
+                                                <tr style={{ backgroundColor: "#dff0d8" }}>
+                                                    <td>www.bikewale.com</td>
+                                                    <td style={{ color: "green" }}><i class="fa fa-circle" aria-hidden="true"></i></td>
+                                                </tr>
+                                                <tr style={{ backgroundColor: "#dff0d8" }}>
+                                                    <td>www.gstatic.com</td>
+                                                    <td style={{ color: "green" }}><i class="fa fa-circle" aria-hidden="true"></i></td>
+                                                </tr>
+                                                <tr style={{ backgroundColor: "#dff0d8" }}>
+                                                    <td>www.youtube.com</td>
+                                                    <td style={{ color: "green" }}><i class="fa fa-circle" aria-hidden="true"></i></td>
+                                                </tr>
+                                                
+                                                <tr style={{ backgroundColor: "#f2dede" }}>
+                                                    <td>downloadfilmyzilla.com</td>
+                                                    <td style={{ color: "red" }}><i class="fa fa-circle" aria-hidden="true"></i></td>
+                                                </tr>
+                                                <tr style={{ backgroundColor: "#f2dede" }}>
+                                                    <td>probiv.in</td>
+                                                    <td style={{ color: "red" }}><i class="fa fa-circle" aria-hidden="true"></i></td>
+                                                </tr>
+                                                <tr style={{ backgroundColor: "#f2dede" }}>
+                                                    <td>https://offerintro.com/BJP2024-Recharge/id=QYntPBDU</td>
+                                                    <td style={{ color: "red" }}><i class="fa fa-circle" aria-hidden="true"></i></td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
                                     </div>
 
 
                                 </div>
-                                {/* <h1>{userData[0].time_stamp.toDate().toLocaleString()}</h1> */}
+
+
                             </div>
-                        </>}
+                            {/* <h1>{userData[0].time_stamp.toDate().toLocaleString()}</h1> */}
+                        </div>
+                        {/* </>} */}
                     </div>
                     <Footer />
                 </div>
